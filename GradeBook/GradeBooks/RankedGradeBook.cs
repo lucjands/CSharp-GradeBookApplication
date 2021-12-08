1d8a72b134e0231d64f330d7d725e0f074db98a3
lucjan <lucjan-_-@live.be>
2021-12-08 12:05:56 +0100

    Refactored the GetLetterGrade method with new logic

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,32 +13,31 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-        public virtual char GetLetterGrade(double averageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
-            Students.Sort(Comparer<Student>.Create((x, y) => x.AverageGrade > y.AverageGrade ? 1 : -1));
+            //Students.Sort(Comparer<Student>.Create((x, y) => x.AverageGrade > y.AverageGrade ? 1 : -1));
+            
             int StudentCount = Students.Count;
 
-            int rank = StudentCount;
-            while (averageGrade <= Students[rank - 1].AverageGrade)
+            int rank = 0;
+            foreach (Student student in Students)
             {
-                rank--;
-
-                if (rank == 0)
-                    break;
+                if (student.AverageGrade <= averageGrade)
+                    rank++;
             }
 
             double percentage = (double)rank / (double)StudentCount;
 
-            if (percentage >= 0.8)
+            if (percentage > 0.8)
                 return 'A';
-            else if (percentage >= 0.6)
+            else if (percentage > 0.6)
                 return 'B';
-            else if (percentage >= 0.4)
+            else if (percentage > 0.4)
                 return 'C';
-            else if (percentage >= 0.2)
+            else if (percentage > 0.2)
                 return 'D';
             else
                 return 'F';
